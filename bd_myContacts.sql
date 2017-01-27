e290253cc3659b05c3a70399d371ebc0ae54ece9
rogerfus <10000712.joan23@fje.edu>
2017-01-27 17:52:20 +0100

    bd con alguns datos

--- bd_myContacts.sql
+++ bd_myContacts.sql
@@ -1,24 +1,38 @@
--- phpMyAdmin SQL Dump
--- version 4.5.2
--- http://www.phpmyadmin.net
 --
--- Servidor: localhost
--- Tiempo de generación: 27-01-2017 a las 00:41:07
--- Versión del servidor: 10.1.16-MariaDB
--- Versión de PHP: 5.6.24
+-- Base de datos: `bd_mycontacts`
+--
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+-- --------------------------------------------------------
 
 --
--- Base de datos: `bd_myContacts`
+-- Estructura de tabla para la tabla `tbl_contacto`
 --
+
+CREATE TABLE `tbl_contacto` (
+  `con_id` int(11) NOT NULL,
+  `usu_id` int(11) NOT NULL,
+  `con_nombre` varchar(50) NOT NULL,
+  `con_apellido1` varchar(50) NOT NULL,
+  `con_apellido2` varchar(50) NOT NULL,
+  `con_correo` varchar(50) NOT NULL,
+  `con_tMovil` int(11) NOT NULL,
+  `con_tFijo` int(11) NOT NULL,
+  `con_tEmpresa` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tbl_direccion`
+--
+
+CREATE TABLE `tbl_direccion` (
+  `dir_id` int(11) NOT NULL,
+  `con_id` int(11) NOT NULL,
+  `dir_residencia` varchar(500) NOT NULL,
+  `dir_lat` int(50) NOT NULL,
+  `dir_lon` int(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -27,22 +41,38 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `tbl_usuario` (
-  `usu_id` int(2) NOT NULL,
-  `usu_name` varchar(15) COLLATE utf8_unicode_ci NOT NULL,
-  `usu_pwd` varchar(15) COLLATE utf8_unicode_ci NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+  `usu_id` int(11) NOT NULL,
+  `usu_nombre` varchar(50) NOT NULL,
+  `usu_pass` varchar(500) NOT NULL,
+  `usu_correo` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Volcado de datos para la tabla `tbl_usuario`
 --
 
-INSERT INTO `tbl_usuario` (`usu_id`, `usu_name`, `usu_pwd`) VALUES
-(1, 'david.marin', 'asdf'),
-(2, 'sergio.jimenez', '1234');
+INSERT INTO `tbl_usuario` (`usu_id`, `usu_nombre`, `usu_pass`, `usu_correo`) VALUES
+(1, 'roger', '123', 'rfuste18@gmail.com'),
+(2, 'sergi', '123', '38256.joan23@fje.edu'),
+(3, 'david', '123', 'david@profe.cat'),
+(4, '', '', ''),
+(5, 'seguio', '123', 'sergio@profe.cat');
 
 --
 -- Índices para tablas volcadas
 --
+
+--
+-- Indices de la tabla `tbl_contacto`
+--
+ALTER TABLE `tbl_contacto`
+  ADD PRIMARY KEY (`con_id`);
+
+--
+-- Indices de la tabla `tbl_direccion`
+--
+ALTER TABLE `tbl_direccion`
+  ADD PRIMARY KEY (`dir_id`);
 
 --
 -- Indices de la tabla `tbl_usuario`
@@ -50,6 +80,25 @@ INSERT INTO `tbl_usuario` (`usu_id`, `usu_name`, `usu_pwd`) VALUES
 ALTER TABLE `tbl_usuario`
   ADD PRIMARY KEY (`usu_id`);
 
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `tbl_contacto`
+--
+ALTER TABLE `tbl_contacto`
+  MODIFY `con_id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `tbl_direccion`
+--
+ALTER TABLE `tbl_direccion`
+  MODIFY `dir_id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `tbl_usuario`
+--
+ALTER TABLE `tbl_usuario`
+  MODIFY `usu_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
