9d5e1b743c97a29db5f71217f9ba5183ecc5f5cd
rogerfus <10000712.joan23@fje.edu>
2017-01-27 20:00:40 +0100

    Validacion js

--- includes/registro.proc.php
+++ includes/registro.proc.php
@@ -9,8 +9,19 @@ include "conexion_bd.php";
         $usu_pass = trim($inputPassword);
         $usu_pass2 = trim($inputPassword2);
         $usu_correo = trim($inputEmail);
+
+        
+
         // ------------------ select ------------------
-            $sql = "SELECT "
+        $sql = "SELECT * FROM `tbl_usuario` WHERE `usu_nombre` = '$usu_nombre' && `usu_pass` = '$usu_pass'";
+
+        $consulta = mysqli_query($conexion, $sql);
+        
+        //Si hay usuario establece su sesión y lo lleva a usuario.php
+        if(mysqli_num_rows($consulta)!=0){
+            $_SESSION['error'] = "El usuario ya existe.";
+            header('Location: registro.php');
+        }else{
         // ------------------ insert ------------------
             $sql1 = "INSERT INTO `tbl_usuario` (`usu_id`, `usu_nombre`, `usu_pass`, `usu_correo`) VALUES (NULL, '". $usu_nombre ."', '". $usu_pass ."', '". $inputEmail ."');";
             //echo $sql;
@@ -21,6 +32,7 @@ include "conexion_bd.php";
         
         
             header('Location: login.php');
+        }
         }
 
     
